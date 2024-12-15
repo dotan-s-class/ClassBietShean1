7e12e54bbdc527feb0a53d8bedb074a203b7f97a
harelluzon132 <harelluzon132@gmail.com>
2024-12-15 19:10:56 +0200

    child hood story

--- dotan_first.py
+++ dotan_first.py
@@ -1,6 +1,6 @@
 def funny():
     print("פלץי לקייק בטיול קייקים ")
 funny()
-def child_hood_storry():
+def child_hood_storryr():
     print("אני אוהב את הזיכרון שמתי שקיבלתי את הכלב שלי אני הייתי ממש קטן ")
-child_hood_storry()
+child_hood_storryr()
